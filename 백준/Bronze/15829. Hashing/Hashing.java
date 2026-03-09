f8c183f41f790eeb354c153aad8ecc5bd200e183
endl24 <154442593+endl24@users.noreply.github.com>
2026-03-09 14:09:49 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14324 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -9,10 +9,12 @@ public class Main {
         int l = Integer.parseInt(br.readLine());
         long hash = 0;
         long m = 1234567891;
+        long r = 1;
         String s = br.readLine();
         for(int i=0;i<l;i++) {
         	char c = s.charAt(i);
-        	hash += (c- 'a'+1)*((long)Math.pow(31, i)) % m;
+        	hash += (c- 'a'+1)*r % m;
+        	r = (r * 31) % m;
         }
         System.out.print(hash);
     }
